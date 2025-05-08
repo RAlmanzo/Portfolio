6fb250aad244cf8b2cd049fcbea8eef8d6469718
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-08 16:05:17 +0200

    Replace main tag html css and ts code from app component to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  @ViewChild('animatedText') animatedText!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const textEl = this.animatedText.nativeElement;
+    const text = textEl.textContent;
+    textEl.textContent = '';
+
+    const spans = [];
+
+    for (const char of text) {
+      const span = this.renderer.createElement('span');
+      span.textContent = char === ' ' ? '\u00A0' : char;
+      this.renderer.setStyle(span, 'display', 'inline-block');
+      this.renderer.appendChild(textEl, span);
+      spans.push(span);
+    }
+
+    spans.forEach((span, i) => {
+      anime({
+        targets: span,
+        translateX: [Math.random() * 800 - 400, 0],
+        translateY: [Math.random() * 800 - 400, 0],
+        opacity: [0, 1],
+        delay: i * 100,
+        duration: 1000,
+        easing: 'easeOutExpo'
+      });
+    });
+  }
 }
