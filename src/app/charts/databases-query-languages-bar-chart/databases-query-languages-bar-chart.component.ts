16057c4c797fbf7a94a36c0a7f9bb95af176d051
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 15:08:03 +0200

    Create new component databases query languages bar chart

---
+++ src/app/charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-databases-query-languages-bar-chart',
+  imports: [],
+  templateUrl: './databases-query-languages-bar-chart.component.html',
+  styleUrl: './databases-query-languages-bar-chart.component.css'
+})
+export class DatabasesQueryLanguagesBarChartComponent {
+
+}
