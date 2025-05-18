6febd7f48fd91d6d3e2006a10b23742dcafe0da3
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 13:40:37 +0200

    Import frameworksplatformsbarchart component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LanguageBarChartComponent } from '../charts/language-bar-chart/language-bar-chart.component';
+import { FrameworksPlatformsBarChartComponent } from '../charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component';
 
 @Component({
   selector: 'app-resume',
-  imports: [LanguageBarChartComponent],
+  imports: [LanguageBarChartComponent, FrameworksPlatformsBarChartComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
