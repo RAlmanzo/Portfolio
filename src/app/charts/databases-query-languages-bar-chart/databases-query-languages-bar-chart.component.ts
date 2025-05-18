bd23fafbda947c1123b2cbdccfc14c86877828fe
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 15:35:54 +0200

    Set databasesquerylanguages chartbardata and config

--- src/app/charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component.ts
+++ src/app/charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ChartBarBuilderService } from '../../services/chart-builder.service.spec';
 import { ChartConfiguration } from 'chart.js';
 
@@ -8,6 +8,59 @@ import { ChartConfiguration } from 'chart.js';
   templateUrl: './databases-query-languages-bar-chart.component.html',
   styleUrl: './databases-query-languages-bar-chart.component.css'
 })
-export class DatabasesQueryLanguagesBarChartComponent {
+export class DatabasesQueryLanguagesBarChartComponent implements AfterViewInit {
+  chart:any;
+  labels = [
+    "MS SQL Server (Relational)",
+    "MongoDB (NoSQL, Cloud DB)",
+    "Firebase (NoSQL, Cloud DB)",
+    "LINQ",
+    "SQL",
+    "GraphQL Queries"
+  ];
+  dataSetData = [85, 75, 75, 90, 75, 80];
+  backgroundColor = 'rgba(75, 192, 192, 0.2)';
+  borderColor = 'rgb(75, 192, 192)';
 
+  chartBarData = {
+    labels: this.labels,
+    datasets: [{
+      data: this.dataSetData,
+      backgroundColor:this.backgroundColor,
+      borderColor: this.borderColor,
+      borderWidth: 1
+    }]
+  };
+
+  config: ChartConfiguration<'bar'> = {
+    type: 'bar',
+    data: this.chartBarData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: { color: 'black', }
+        },
+        x: {
+          ticks: { 
+            color: 'black',
+            font: {
+              size: 14
+            } 
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    },
+  };
+
+  constructor(private chartBarBuilder: ChartBarBuilderService,) {}
+
+  ngAfterViewInit(): void {
+    this.chart = this.chartBarBuilder.createBarChart(this.config);
+  }
 }
