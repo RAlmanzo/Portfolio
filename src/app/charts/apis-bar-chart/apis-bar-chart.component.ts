f7427ece1cd8d633e4a40f865e87d8e23021d4c1
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 16:35:36 +0200

    Create new component apisbarchart

---
+++ src/app/charts/apis-bar-chart/apis-bar-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apis-bar-chart',
+  imports: [],
+  templateUrl: './apis-bar-chart.component.html',
+  styleUrl: './apis-bar-chart.component.css'
+})
+export class ApisBarChartComponent {
+
+}
