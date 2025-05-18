a9b1bed71796fbadc79e5e2f508cdae28e212ce8
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 17:38:34 +0200

    Set charbardata for toolsversioncontrolbarchart

--- src/app/charts/tools-versioncontrol-bar-chart/tools-versioncontrol-bar-chart.component.ts
+++ src/app/charts/tools-versioncontrol-bar-chart/tools-versioncontrol-bar-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ChartBarBuilderService } from '../../services/chart-builder.service';
 
 @Component({
   selector: 'app-tools-versioncontrol-bar-chart',
@@ -6,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './tools-versioncontrol-bar-chart.component.html',
   styleUrl: './tools-versioncontrol-bar-chart.component.css'
 })
-export class ToolsVersioncontrolBarChartComponent {
+export class ToolsVersioncontrolBarChartComponent implements AfterViewInit {
+  chart:any;
+  labels = [
+    "GitHub",
+    "GitLab",
+    "Git",
+    "Yarn",
+    "Npm",
+    "Swagger"
+  ];
+  dataSetData = [85, 75, 80, 65, 65, 90];
+  backgroundColor = 'rgba(255, 205, 86, 0.2)';
+  borderColor = 'rgb(255, 205, 86)';
 
+  chartBarData = {
+    labels: this.labels,
+    datasets: [{
+      data: this.dataSetData,
+      backgroundColor:this.backgroundColor,
+      borderColor: this.borderColor,
+      borderWidth: 1
+    }]
+  };
+
+  constructor(private chartBarBuilder: ChartBarBuilderService,) {}
+
+  ngAfterViewInit(): void {
+    this.chart = this.chartBarBuilder.createBarChart(this.chartBarData);
+  }
 }
