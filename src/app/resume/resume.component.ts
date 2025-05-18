7d37911f8eb1abe9d94d5ad5cbf55a557d65681a
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 16:42:28 +0200

    Import apisbarchartcomponent

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { LanguageBarChartComponent } from '../charts/language-bar-chart/language-bar-chart.component';
 import { FrameworksPlatformsBarChartComponent } from '../charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component';
 import { DatabasesQueryLanguagesBarChartComponent } from '../charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component';
+import { ApisBarChartComponent } from '../charts/apis-bar-chart/apis-bar-chart.component';
 
 @Component({
   selector: 'app-resume',
-  imports: [LanguageBarChartComponent, FrameworksPlatformsBarChartComponent, DatabasesQueryLanguagesBarChartComponent],
+  imports: [LanguageBarChartComponent, FrameworksPlatformsBarChartComponent, DatabasesQueryLanguagesBarChartComponent, ApisBarChartComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  barChart = "LanguagesChart";
+  barChart = "languagesChart";
 
   toggleBarCharts(value:string){
     this.barChart = value;
