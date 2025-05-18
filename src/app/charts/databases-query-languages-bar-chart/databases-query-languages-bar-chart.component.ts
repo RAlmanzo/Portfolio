e82eebd9a11b8d68cc40e6debdca04aeb9d0fd30
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 15:10:37 +0200

    Import hooks charbarbuildersservice and chartconfiguration

--- src/app/charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component.ts
+++ src/app/charts/databases-query-languages-bar-chart/databases-query-languages-bar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartBarBuilderService } from '../../services/chart-builder.service.spec';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-databases-query-languages-bar-chart',
