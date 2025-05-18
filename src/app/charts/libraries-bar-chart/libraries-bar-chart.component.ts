793cfc636d776faef6e128396b08e5960178c728
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 18:00:26 +0200

    Set barchartdata for libraries component

--- src/app/charts/libraries-bar-chart/libraries-bar-chart.component.ts
+++ src/app/charts/libraries-bar-chart/libraries-bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ChartBarBuilderService } from '../../services/chart-builder.service';
 
 @Component({
@@ -7,6 +7,31 @@ import { ChartBarBuilderService } from '../../services/chart-builder.service';
   templateUrl: './libraries-bar-chart.component.html',
   styleUrl: './libraries-bar-chart.component.css'
 })
-export class LibrariesBarChartComponent {
+export class LibrariesBarChartComponent implements AfterViewInit {
+  chart:any;
+  labels = [
+    "Bootstrap",
+    "Anime.js",
+    "Chart.js",
+    "SignalR"
+  ];
+  dataSetData = [65, 55, 55, 75];
+  backgroundColor = 'rgba(145, 255, 102, 0.2)';
+  borderColor = 'rgb(105, 255, 102)';
 
+  chartBarData = {
+    labels: this.labels,
+    datasets: [{
+      data: this.dataSetData,
+      backgroundColor:this.backgroundColor,
+      borderColor: this.borderColor,
+      borderWidth: 1
+    }]
+  };
+
+  constructor(private chartBarBuilder: ChartBarBuilderService,) {}
+
+  ngAfterViewInit(): void {
+    this.chart = this.chartBarBuilder.createBarChart(this.chartBarData);
+  }
 }
