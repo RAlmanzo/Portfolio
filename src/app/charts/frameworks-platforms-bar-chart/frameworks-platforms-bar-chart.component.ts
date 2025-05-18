2a1e5b83719ba05437e5de214a2e180bcbf720b6
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 14:44:14 +0200

    Set data and config to create bar chart for frameworks and platforms

--- src/app/charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component.ts
+++ src/app/charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ChartBarBuilderService } from '../../services/chart-builder.service.spec';
 import { ChartConfiguration } from 'chart.js';
 
@@ -8,6 +8,64 @@ import { ChartConfiguration } from 'chart.js';
   templateUrl: './frameworks-platforms-bar-chart.component.html',
   styleUrl: './frameworks-platforms-bar-chart.component.css'
 })
-export class FrameworksPlatformsBarChartComponent {
+export class FrameworksPlatformsBarChartComponent implements AfterViewInit {
+  chart:any;
+  labels = [
+  ".NET (WPF, Console)",
+  "ASP.NET Core MVC",
+  "ASP.NET Core Web API",
+  "EF Core",
+  "Microsoft Identity",
+  "Angular 19",
+  "React.js",
+  "Vue.js",
+  "Blazor (Server)",
+  "Hot Chocolate (GraphQL C#)",
+  "Xamarin Forms (MVVM)"
+];
+  dataSetData = [90, 91, 91, 90, 90, 70, 50, 80, 85, 70, 85];
+  backgroundColor = 'rgba(255, 159, 64, 0.2)';
+  borderColor = 'rgb(255, 159, 64)';
 
+  chartBarData = {
+    labels: this.labels,
+    datasets: [{
+      data: this.dataSetData,
+      backgroundColor:this.backgroundColor,
+      borderColor: this.borderColor,
+      borderWidth: 1
+    }]
+  };
+
+  config: ChartConfiguration<'bar'> = {
+    type: 'bar',
+    data: this.chartBarData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: { color: 'black', }
+        },
+        x: {
+          ticks: { 
+            color: 'black',
+            font: {
+              size: 14
+            } 
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    },
+  };
+
+  constructor(private chartBarBuilder: ChartBarBuilderService,) {}
+
+  ngAfterViewInit(): void {
+    this.chart = this.chartBarBuilder.createBarChart(this.config);
+  }
 }
