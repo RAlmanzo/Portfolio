7b146ad18b7f61a181555e921e60cd7881df6267
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-18 13:41:55 +0200

    Import hooks chartbuilderservice and chartconfiguration

--- src/app/charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component.ts
+++ src/app/charts/frameworks-platforms-bar-chart/frameworks-platforms-bar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartBarBuilderService } from '../../services/chart-builder.service.spec';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-frameworks-platforms-bar-chart',
