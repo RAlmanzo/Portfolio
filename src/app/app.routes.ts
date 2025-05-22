d9fe68e5137c358163157f8118dc3aee8597f1fd
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-22 11:40:02 +0200

    Add patch to routes for projects component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'resume', component: ResumeComponent },
+    { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },
 ];
