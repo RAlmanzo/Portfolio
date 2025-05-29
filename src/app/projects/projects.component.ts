b3c5c6770d33af2b62fe9bf6b440f1d0f9401aa1
Almanzo Rosseel <ralmanzo@gmail.com>
2025-05-29 09:38:12 +0200

    Use imageanimator to use animate projects images

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { AnimatorService } from '../services/animator.service';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
@@ -12,43 +12,43 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent implements AfterViewInit{
-  
+  @ViewChildren('imageContainer') containers!: QueryList<ElementRef>;
 
-  portfolioImages = [
-    'assets/images/portfolio1.png',
-    'assets/images/portfolio2.png',
-  ];
-  liveSportsImages = [
-    'assets/images/reporter.png',
-    'assets/images/user.png',
-  ];
-  sniffHikesWebAppImages = [
-    'assets/images/sniffhikesWebApp.png',
-  ];
-  sniffHikesMobileAppImages = [
-    'assets/images/sniffhikesmob3.png',
-    'assets/images/sniffhikesmob2.png',
-    'assets/images/sniffhikesmob1.png',
+  imageSets: string[][] = [
+    ['assets/images/portfolio1.png', 'assets/images/portfolio2.png'],
+    ['assets/images/reporter.png', 'assets/images/user.png'],
+    ['assets/images/sniffhikesWebApp.png', 'assets/images/sniffhikesWebApp2.png', 'assets/images/sniffhikesWebApp3.png', 'assets/images/sniffhikesWebApp4.png',],
+    ['assets/images/sniffhikesmob3.png', 'assets/images/sniffhikesmob2.png', 'assets/images/sniffhikesmob1.png'],
   ];
 
+  currentIndexes = Array(this.imageSets.length).fill(0);
   
   constructor (
     private imageAnimator: AnimatorService,
   ){}
 
   ngAfterViewInit() {
-    this.imageAnimator.animateSlideImages(this.portfolioImages);
-    this.imageAnimator.animateSlideImages(this.liveSportsImages);
-    this.imageAnimator.animateSlideImages(this.sniffHikesWebAppImages);
-    this.imageAnimator.animateSlideImages(this.sniffHikesMobileAppImages);
+      this.containers.forEach((container, index) => {
+      const images = this.imageSets[index];
 
-    let matchMedia = gsap.matchMedia();
+      if (images) {
+        this.imageAnimator.startImageAnimationLoop(
+          container.nativeElement,
+          images.length,
+          (i) => (this.currentIndexes[index] = i)
+        );
+      } 
+      else {
+        console.warn(`No images found for container index ${index}`);
+      }
+    });
 
-    matchMedia.add("(min-width: 992px) and (min-height: 740px)", () => {
-      gsap.utils.toArray(".projects").forEach((box, i) => {
+    const matchMedia = gsap.matchMedia();
+    matchMedia.add('(min-width: 992px) and (min-height: 740px)', () => {
+      gsap.utils.toArray('.projects').forEach((box) => {
         ScrollTrigger.create({
           trigger: box as HTMLElement,
-          start: "top top",
+          start: 'top top',
           pin: true,
           pinSpacing: false,
         });
