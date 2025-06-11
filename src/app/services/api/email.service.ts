8edd92fc1c5fefbee3c4fe9f1ec816591cfa233d
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-11 18:23:01 +0200

    Create function sendmessage

--- src/app/services/api/email.service.ts
+++ src/app/services/api/email.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EmailData } from '../../models/emailData.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
+  contactUrl = `${environment.apiUrl}contacts`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendMessage(data: EmailData) {
+     return this.http.post(this.contactUrl, data);
+  }
 }
