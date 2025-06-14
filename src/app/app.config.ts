2ec9cd716dd709345c4f38da7d24a8aea3ddc2d3
Almanzo Rosseel <ralmanzo@gmail.com>
2025-06-14 15:53:13 +0200

    Add animations and toastr to providers array

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +16,7 @@ export const appConfig: ApplicationConfig = {
             registrationStrategy: 'registerWhenStable:30000'
     }),
     provideHttpClient(),
+    provideAnimations(),
+    provideToastr(),
   ]
 };
