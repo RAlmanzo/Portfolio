86edfbff670d1c67c381dbc563cfa24a359857ad
Almanzo Rosseel <ralmanzo@gmail.com>
2025-10-25 15:14:44 +0200

    Set lazy loading for about resume projects components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ResumeComponent } from './resume/resume.component';
+//import { AboutComponent } from './about/about.component';
+//import { ResumeComponent } from './resume/resume.component';
 //import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
+//import { ProjectsComponent } from './projects/projects.component';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'resume', component: ResumeComponent },
-    { path: 'projects', component: ProjectsComponent },
+    {
+        path: 'about',
+        loadComponent: () => import('./about/about.component')
+            .then((c) => c.AboutComponent)
+    },
+    {
+        path: 'resume',
+        loadComponent: () => import('./resume/resume.component')
+            .then((c) => c.ResumeComponent)
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import('./projects/projects.component')
+            .then((c) => c.ProjectsComponent),
+    },
     {
         path: 'contact',
         loadComponent: () => import('./contact/contact.component')
