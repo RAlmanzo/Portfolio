ad85a4e31ae28ddf68049ed549e042327336476b
Almanzo Rosseel <ralmanzo@gmail.com>
2025-11-12 18:52:08 +0100

    Remove unused routes and add new root route landingpage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { provideRouter, Routes, withInMemoryScrolling } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppComponent } from './app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+//import { HomeComponent } from './home/home.component';
 //import { AboutComponent } from './about/about.component';
 //import { ResumeComponent } from './resume/resume.component';
 //import { ContactComponent } from './contact/contact.component';
 //import { ProjectsComponent } from './projects/projects.component';
 
-import { provideAnimations } from '@angular/platform-browser/animations';
+//import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    {
+    { path: '', component: LandingPageComponent },
+    /*{
         path: 'about',
         loadComponent: () => import('./about/about.component')
             .then((c) => c.AboutComponent)
@@ -23,10 +26,22 @@ export const routes: Routes = [
         path: 'projects',
         loadComponent: () => import('./projects/projects.component')
             .then((c) => c.ProjectsComponent),
-    },
+    },*/
     {
         path: 'contact',
         loadComponent: () => import('./contact/contact.component')
             .then((c) => c.ContactComponent),
     },
 ];
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideRouter(
+            routes,
+            withInMemoryScrolling({
+                anchorScrolling: 'enabled',
+                scrollPositionRestoration: 'enabled'
+            })
+        )
+    ]
+});
