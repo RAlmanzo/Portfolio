2a284eb9574218df35054c1fb047699a13fbee19
Almanzo Rosseel <ralmanzo@gmail.com>
2025-12-01 15:05:44 +0100

    Set lazy loading for landingpagecomponent route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 
 //import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
+    {
+        path: '',
+        loadComponent: () => import('./landing-page/landing-page.component')
+            .then((c) => c.LandingPageComponent),
+    },
     {
         path: 'contact',
         loadComponent: () => import('./contact/contact.component')
